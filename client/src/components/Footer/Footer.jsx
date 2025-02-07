d26b38b7d054bfcabb2512cfa672ccbfcecbfaf9
asif <asiftanvir2006@gmail.com>
2025-02-07 21:52:19 +0530

    Mobile View: Fixed mobile view

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import { Button } from "antd";
-import { LinkedinOutlined, GithubOutlined, InstagramOutlined, MailOutlined} from '@ant-design/icons'
+import { LinkedinOutlined, GithubOutlined, InstagramOutlined, MailOutlined } from '@ant-design/icons'
 const Footer = () => {
   return (
     // <footer>foot</footer>
@@ -24,12 +24,30 @@ const Footer = () => {
           </div>
         </div> */}
         <div className="Lower">
-          <div className="Left-Box">
-            <div className="Title">
-              IIEST, Shibpur
 
+          <div className="Left-Box">
+            <div className='Logo_mob'>
+              <div className="Image"></div>
             </div>
-            Botanical garden area, Shibpur, 700013
+            <div className="mob_middle">
+              <div className='wrapper_left'>
+
+                <div className="Title">
+                  <div className="fixedImage">
+
+                  </div>
+                  IIEST, Shibpur
+
+                </div>
+                <div className='address'>
+                  Botanical garden area,
+                  Shibpur, 700013
+                </div>
+
+
+              </div>
+              <div className="wrapper_right"></div>
+            </div>
           </div>
           <div className="Mid-Box">
             <div className='Image'>
@@ -39,7 +57,7 @@ const Footer = () => {
                 copyright 2024 SoCIT
               </div>
               <div className="Right">
-                <Button type='primary' icon={<LinkedinOutlined/>}></Button>
+                <Button type='primary' icon={<LinkedinOutlined />}></Button>
                 <Button type='primary' icon={<GithubOutlined />}></Button>
                 <Button type='primary' icon={<InstagramOutlined />}></Button>
                 <Button type='primary' icon={<MailOutlined />}></Button>
@@ -48,20 +66,23 @@ const Footer = () => {
             </div>
           </div>
           <div className="Right-Box">
-            <div className="Links">
-              <div className='Title'>Quick Links</div>
-              <div className="item">Home</div>
-              <div className="item">Home</div>
-              <div className="item">Home</div>
-              <div className="item">Home</div>
+            <div className="wrapper">
+              <div className="Links">
+                <div className='Title'>Quick Links</div>
+                <div className="item">Home</div>
+                <div className="item">Home</div>
+                <div className="item">Home</div>
+                <div className="item">Home</div>
+              </div>
+              <div className="Links" id='shift_right'>
+                <div className='Title'>Developers</div>
+                <div className="item">Asif</div>
+                <div className="item">Asif</div>
+                <div className="item">Asif</div>
+                <div className="item">Asif</div>
+              </div>
             </div>
-            <div className="Devs">
-              <div className='Title'>Developers</div>
-              <div className="item">Asif</div>
-              <div className="item">Asif</div>
-              <div className="item">Asif</div>
-              <div className="item">Asif</div>
-            </div>
+
           </div>
         </div>
         {/* <div className='Left-Box'>
