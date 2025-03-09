94c7eb03aa5e2c8d422db22d2d0330e75cab82aa
asif <asiftanvir2006@gmail.com>
2025-03-09 19:10:27 +0530

    Made some important changes

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
-
+import "./Hero.css"
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className="Hero-Frame">
+      <div className="Hero">
+        <div className="left">
+          <div className="upper">
+            <h1>Our Mission</h1>
+            <div className="text">some lorem text to create importance and make everyone believe we have cool content coming up</div>
+          </div>
+          <div className="lower"></div>
+        </div>
+        <div className="middle">
+          <div className="logo"></div>
+          <div className="buffer"></div>
+          <div className="font">Students'
+            <div className='lower'>
+              Corner
+
+            </div>
+          </div>
+          <div className="buffer"></div>
+        </div>
+        <div className="right">
+          <div className="upper"></div>
+          <div className="lower">
+            <h1>Our Mission</h1>
+            <div className="text">some lorem text to create importance and make everyone believe we have cool content coming up</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
