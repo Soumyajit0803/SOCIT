42bb5e67bfdba09d65afba6c0b4c1fcfef267144
asif <asiftanvir2006@gmail.com>
2025-03-15 12:40:10 +0530

    Fixed height issues, changed directory structure

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,37 +1,8 @@
 import React from 'react'
-import "./Hero.css"
+import Hero from '../../components/Hero/Hero'
 const Homepage = () => {
   return (
-    <div className="Hero-Frame">
-      <div className="Hero">
-        <div className="left">
-          <div className="upper">
-            <h1>Our Mission</h1>
-            <div className="text">some lorem text to create importance and make everyone believe we have cool content coming up</div>
-          </div>
-          <div className="lower"></div>
-        </div>
-        <div className="middle">
-          <div className="logo"></div>
-          <div className="buffer"></div>
-          <div className="font">Students'
-            <div className='lower'>
-              Corner
-
-            </div>
-          </div>
-          <div className="buffer"></div>
-        </div>
-        <div className="right">
-          <div className="upper"></div>
-          <div className="lower">
-            <h1>Our Mission</h1>
-            <div className="text">some lorem text to create importance and make everyone believe we have cool content coming up</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
+   <Hero/>
   )
 }
 
