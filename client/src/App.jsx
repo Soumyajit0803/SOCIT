16a84afb1c19eccdd532ec7bfe526a6e1a60919b
Abhijit <artistrup07@gmail.com>
2025-03-17 12:40:43 +0530

    [UPDATE]: App re-structure & antd global theme setting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,32 @@ import { BrowserRouter } from "react-router-dom";
 import AllRoutes from "./Routes";
 import AppHeader from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-function App() {
+import { ConfigProvider, theme } from "antd";
 
+function App() {
     return (
-        <>
-            <BrowserRouter>
-                <AppHeader />
-                <AllRoutes />
-                <Footer />
-            </BrowserRouter>
-        </>
+        <ConfigProvider
+            theme={{
+                algorithm: theme.darkAlgorithm,
+                token: {
+                    colorPrimary: "#93b256",
+                },
+            }}
+        >
+            <div
+                className="app"
+                style={{
+                    margin: "var(--appmargin)",
+                    boxShadow: "inset 0 0 0 2px var(--green)",
+                }}
+            >
+                <BrowserRouter>
+                    <AppHeader />
+                    <AllRoutes />
+                    <Footer />
+                </BrowserRouter>
+            </div>
+        </ConfigProvider>
     );
 }
 
--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import Hero from '../../components/Hero/Hero'
+import React from "react";
+import Hero from "../../components/Hero/Hero";
+import NumStats from "../../components/NumStats/NumStats";
 const Homepage = () => {
-  return (
-   <Hero/>
-  )
-}
+    return (
+        <>
+            <Hero />
+            <NumStats />
+        </>
+    );
+};
 
-export default Homepage
+export default Homepage;
