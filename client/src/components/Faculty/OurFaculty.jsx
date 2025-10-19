77059caf03c56703a783a9121c6ee4111b540c19
Soumyajit Karmakar <sona832004@gmail.com>
2025-10-19 21:00:55 +0530

    [UPDATE]: Design fixes on faculty page

--- client/src/components/Faculty/OurFaculty.jsx
+++ client/src/components/Faculty/OurFaculty.jsx
@@ -1,33 +1,3 @@
-import React from "react";
-// import "./OurFaculty.css";
-
-// const OurFaculty = () => {
-//   return (
-//     <>
-//       <div className="content-wrapper-1">
-//         <div className="topic-1">Our Esteemed Faculty</div>
-//         <div className="grid-1">
-          
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//           <div className="faculty-1"></div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default OurFaculty;
-
 import "./OurFaculty.css"
 import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 // Faculty data structure - easily replaceable
