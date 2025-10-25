bd0449ef6456e391fefaba4d2a3053ca02b5e8ba
Anshika Gupta <ansrkghamirpur@gmail.com>
2025-10-25 12:59:49 +0530

    Faculty details added

--- client/src/components/Faculty/OurFaculty.jsx
+++ client/src/components/Faculty/OurFaculty.jsx
@@ -34,67 +34,121 @@ import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 const facultyData = [
   {
     id: 1,
-    name: "Soumyajit Karmakar",
-    designation: "Assistant Professor",
-    phone: "+91 9876543210",
-    emails: ["soumyajit@socit.edu", "s.karmakar@university.edu"],
-    image: "/assets/faculty.jpg",
+    name: "Arindam Biswas",
+    designation: "Dean, Academic Professor",
+    phone: "+91 33 26680525/0526 (2 lines) x 3003(O)",
+    emails: ["abiswas@it.iiests.ac.in", "barindam@gmail.com"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/66cc2514dd20fAB.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-abiswas"
   },
   {
     id: 2,
-    name: "Soumyajit Karmakar",
-    designation: "Associate Professor",
-    phone: "+91 9876543211",
-    emails: ["soumyajit2@socit.edu", "s.karmakar2@university.edu"],
-    image: "/assets/faculty.jpg",
+    name: "Hafizur Rahaman",
+    designation: "Professor",
+    phone: "+91 33 26684561(3 lines) x309/249 (O)",
+    emails: ["rahaman_h@it.iiests.ac.in", "hafizur@vlsi.iiests.ac.in"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/1.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-rahaman_h"
   },
   {
     id: 3,
-    name: "Soumyajit Karmakar",
-    designation: "Professor",
-    phone: "+91 9876543212",
-    emails: ["soumyajit3@socit.edu", "s.karmakar3@university.edu"],
-    image: "/assets/faculty.jpg",
+    name: "Tuhina Samanta",
+    designation: "Head of Department",
+    phone: " +91 33 26684561(3 lines) x260 (O)",
+    emails: [" t_samanta@it.iiests.ac.in"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/5.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-t_samanta"
+
   },
   {
     id: 4,
-    name: "Soumyajit Karmakar",
-    designation: "Assistant Professor",
-    phone: "+91 9876543213",
-    emails: ["soumyajit4@socit.edu", "s.karmakar4@university.edu"],
-    image: "/assets/faculty.jpg",
+    name: "Indrajit Banerjee",
+    designation: "Professor",
+    phone: "+91 33 26684561-63",
+    emails: ["ibanerjee@it.iiests.ac.in", "ibanerjee.it@faculty.iiests.ac.in"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/6315a272e24541662109870016.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-indrajit"
+    
   },
   {
     id: 5,
-    name: "Soumyajit Karmakar",
+    name: "Chandan Giri",
     designation: "Associate Professor",
-    phone: "+91 9876543214",
-    emails: ["soumyajit5@socit.edu", "s.karmakar5@university.edu"],
-    image: "/assets/faculty.jpg",
+    phone: " +91 33 26684561(3 lines) x260 (O)",
+    emails: ["chandan@it.iiests.ac.in", "chandangiri@gmail.com"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/2.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-chandan"
   },
   {
     id: 6,
-    name: "Soumyajit Karmakar",
-    designation: "Professor",
-    phone: "+91 9876543215",
-    emails: ["soumyajit6@socit.edu", "s.karmakar6@university.edu"],
-    image: "/assets/faculty.jpg",
+    name: "Surajit Kumar Roy",
+    designation: "Associate Professor",
+    phone: "+91 33 26684561(3 lines) x260 (O)",
+    emails: ["suraroy@it.iiests.ac.in", "suraroy@gmail.com"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/4.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-suraroy"
   },
   {
     id: 7,
-    name: "Soumyajit Karmakar",
+    name: "Binanda Sengupta",
     designation: "Assistant Professor",
-    phone: "+91 9876543216",
-    emails: ["soumyajit7@socit.edu", "s.karmakar7@university.edu"],
-    image: "/assets/faculty.jpg",
+    phone: "+91 33 26684561",
+    emails: ["binanda@it.iiests.ac.in", "binanda.it@faculty.iiests.ac.in"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/65d9cb50c3233Binanda.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-binanda"
   },
   {
     id: 8,
-    name: "Soumyajit Karmakar",
+    name: "Santi Prasad Maity",
+    designation: "Professor",
+    phone: "+91 33 26684561(3 lines) x858 (O)",
+    emails: ["santipmaity@it.iiests.ac.in", "spmaity@yahoo.com"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/64cfb3c37f802spmaity1.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-santipmaity"
+  },
+  {
+    id: 9,
+    name: "Sukanta Das",
     designation: "Associate Professor",
-    phone: "+91 9876543217",
-    emails: ["soumyajit8@socit.edu", "s.karmakar8@university.edu"],
-    image: "/assets/faculty.jpg",
+    phone: "+91 33 26684561(3 lines) x260 (O)",
+    emails: [" sukanta@it.iiests.ac.in", "sukanta78das@yahoo.com"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/61fba883bbf70download.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-sukanta"
+  },
+  {
+    id: 10,
+    name: "Shyamalendu Kandar",
+    designation: "Assistant Professor",
+    phone: "+91 33 26684561 x260 (O)",
+    emails: ["shyamalenduk@it.iiests.ac.in"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/103.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-shyamalenduk"
+  },
+  {
+    id: 11,
+    name: "Ruchira Naskar",
+    designation: "Associate Dean, PG & PhD Associate Professor",
+    phone: "+91 33 26684561",
+    emails: [" ruchira@it.iiests.ac.in", "ruchira.naskar@gmail.com"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/5e996a26322c3us.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-ruchira"
+  },
+  {
+    id: 12,
+    name: "Prasun Ghosal",
+    designation: "Associate Professor",
+    phone: "+91 33 26684561(3 lines) x260 (O)",
+    emails: ["p_ghosal@it.iiests.ac.in", "p_ghosal.it@faculty.iiests.ac.in"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/6225bc037c1d0prasun_profile_pic2.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-p_ghosal"
+  },
+  {
+    id: 13,
+    name: "Basabdatta Palit",
+    designation: "Assistant Professor",
+    emails: ["basabdatta@it.iiests.ac.in"],
+    image: "https://www.iiests.ac.in/assets/images/faculty/68aaaa2f5a361IMG-20250814-WA0138~2.jpg",
+    page: "https://www.iiests.ac.in/IIEST/Faculty/it-basabdatta"
   },
 ]
 
@@ -113,9 +167,12 @@ const OurFaculty = () => {
                 <h3 className="faculty-name">{faculty.name}</h3>
                 <p className="faculty-designation">{faculty.designation}</p>
                 <div className="faculty-contact">
-                  <div className="contact-item">
-                    <FaPhoneAlt className="icon" /> {faculty.phone}
-                  </div>
+                  {faculty.phone && (
+                    <div className="contact-item">
+                      <FaPhoneAlt className="icon" /> {faculty.phone}
+                    </div>
+                  )}
+                  
 
                   {faculty.emails.map((email, index) => (
                     <div key={index} className="contact-item">
@@ -123,7 +180,7 @@ const OurFaculty = () => {
                     </div>
                   ))}
                 </div>
-                <a href="#" className="learn-more">
+                <a href={faculty.page} className="learn-more">
                   Learn More
                 </a>
               </div>
@@ -136,3 +193,5 @@ const OurFaculty = () => {
 }
 
 export default OurFaculty
+
+
--- client/src/components/Faculty/Project.jsx
+++ client/src/components/Faculty/Project.jsx
@@ -1,27 +1,84 @@
-import React from 'react'
-import "./Project.css"
+// import React from 'react'
+// import "./Project.css"
+
+// const Project = () => {
+//   return (
+//     <>
+//    <div className="content-wrapper-2">
+//         <div className="topic-2">Projects</div>
+//         <div className="grid-2">
+//           <div className="project-1"><p>Some very very long complicated cool-looking project name</p></div>
+//           <div className="project-1"></div>
+//           <div className="project-1"></div>
+//           <div className="project-1"></div>
+//           <div className="project-1"></div>
+//           <div className="project-1"></div>
+//           <div className="project-1"></div>
+//           <div className="project-1"></div>
+       
+//         </div>
+//          <div className="list"><a href="">See full list</a></div>
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default Project
+
+
+import React, { useState } from "react";
+import "./Project.css";
+import { projectData } from "./ProjectData";
 
 const Project = () => {
+  const [showAll, setShowAll] = useState(false);
+
+  // Show 10 projects initially, or all if showAll is true
+  const projectsToShow = showAll ? projectData : projectData.slice(0, 10);
+
   return (
-    <>
-   <div className="content-wrapper-2">
-        <div className="topic-2">Projects</div>
-        <div className="grid-2">
-          <div className="project-1"><p>Some very very long complicated cool-looking project name</p></div>
-          <div className="project-1"></div>
-          <div className="project-1"></div>
-          <div className="project-1"></div>
-          <div className="project-1"></div>
-          <div className="project-1"></div>
-          <div className="project-1"></div>
-          <div className="project-1"></div>
-       
-        </div>
-         <div className="list"><a href="">See full list</a></div>
+    <div className="content-wrapper-2">
+      <div className="topic-2">Projects</div>
+      
+      <div className="grid-2">
+        {projectsToShow.map((proj) => (
+          <div key={proj.id} className="project-1">
+            <p className="project-title">{proj.id}. {proj.title}</p>
+            {proj.fundingAgent && (
+              <p className="project-detail">
+                <strong>Funding Agent:</strong> {proj.fundingAgent}
+              </p>
+            )}
+            {proj.pi && (
+              <p className="project-detail">
+                <strong>PI:</strong> {proj.pi}
+              </p>
+            )}
+            {proj.coPi && (
+              <p className="project-detail">
+                <strong>Co-PI:</strong> {proj.coPi}
+              </p>
+            )}
+            {proj.duration && (
+              <p className="project-detail">
+                <strong>Duration:</strong> {proj.duration}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
 
-    </>
-  )
-}
+      <div className="list">
+        <a
+          className="see-full-btn"
+          onClick={() => setShowAll(!showAll)}
+        >
+          {showAll ? "Hide the list" : "See full list"}
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Project
+export default Project;
