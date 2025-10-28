08d28764e07b171a412471eba470de7138831162
Prayas Sinha <ralphraout@gmail.com>
2025-10-28 13:41:24 +0530

    Added Recruiters Grid

---
+++ client/src/components/Placements/RecruitersCmp.jsx
@@ -0,0 +1,30 @@
+import "./RecruitersCmp.css"
+import Card from "antd/es/card/Card";
+export default function RecruitersCmp(){
+    const recruiterData = [
+
+    ]
+    return (<>
+        <div className="TitleText">
+            Our Recruiters
+        </div>
+        <div className="recGrid">
+            <div className="recItem first-row-bs first-row-ss"></div>
+            <div className="recItem first-row-bs first-row-ss"></div>
+            <div className="recItem first-row-bs"></div>
+            <div className="recItem first-row-bs"></div>
+            <div className="recItem first-row-bs"></div>
+            <div className="recItem"></div>
+            <div className="recItem"></div>
+            <div className="recItem"></div>
+            <div className="recItem"></div>
+            <div className="recItem"></div>
+            <div className="recItem"></div>
+            <div className="recItem"></div>
+            <div className="recItem last-row-bs"></div>
+            <div className="recItem last-row-bs"></div>
+            <div className="recItem last-row-bs last-row-ss"></div>
+            <div className="recItem last-row-bs last-row-ss"></div>
+        </div>
+    </>);
+}
--- client/src/pages/Placements/Placements.jsx
+++ client/src/pages/Placements/Placements.jsx
@@ -1,12 +1,14 @@
 import Hero from "../../components/Placements/HeroPlacements";
 import KeyHighlights from "../../components/Placements/KeyHighlights";
 import SalaryStats from "../../components/Placements/SalaryStats";
+import RecruitersCmp from "../../components/Placements/RecruitersCmp";
 export default function Placements(){
     return(
         <>
         <Hero/>
         <KeyHighlights/>
         <SalaryStats/>
+        <RecruitersCmp/>
         </>
     )
 }
