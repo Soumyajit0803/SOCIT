d1154383b6757d66ae614b2ab1c4f641603d3c0b
Prayas Sinha <ralphraout@gmail.com>
2025-10-28 13:39:10 +0530

    Fix:Placements Page

--- client/src/pages/Placements/Placements.jsx
+++ client/src/pages/Placements/Placements.jsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/Placements/HeroPlacements";
 import KeyHighlights from "../../components/Placements/KeyHighlights";
+import SalaryStats from "../../components/Placements/SalaryStats";
 export default function Placements(){
     return(
         <>
         <Hero/>
         <KeyHighlights/>
+        <SalaryStats/>
         </>
     )
 }
