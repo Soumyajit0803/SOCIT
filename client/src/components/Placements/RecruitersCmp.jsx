f476ab6319bd22ddb837463e14a03ba2efa8f1a6
Abhijit <artistrup07@gmail.com>
2025-11-07 02:51:39 +0530

    [UPDATE]: Recruiters

--- client/src/components/Placements/RecruitersCmp.jsx
+++ client/src/components/Placements/RecruitersCmp.jsx
@@ -1,30 +1,45 @@
-import "./RecruitersCmp.css"
-import Card from "antd/es/card/Card";
-export default function RecruitersCmp(){
-    const recruiterData = [
-
-    ]
-    return (<>
-        <div className="TitleText">
-            Our Recruiters
-        </div>
-        <div className="recGrid">
-            <div className="recItem first-row-bs first-row-ss"></div>
-            <div className="recItem first-row-bs first-row-ss"></div>
-            <div className="recItem first-row-bs"></div>
-            <div className="recItem first-row-bs"></div>
-            <div className="recItem first-row-bs"></div>
-            <div className="recItem"></div>
-            <div className="recItem"></div>
-            <div className="recItem"></div>
-            <div className="recItem"></div>
-            <div className="recItem"></div>
-            <div className="recItem"></div>
-            <div className="recItem"></div>
-            <div className="recItem last-row-bs"></div>
-            <div className="recItem last-row-bs"></div>
-            <div className="recItem last-row-bs last-row-ss"></div>
-            <div className="recItem last-row-bs last-row-ss"></div>
-        </div>
-    </>);
+import { Card, Tooltip } from "antd";
+import "./RecruitersCmp.css";
+const recruiterData = [
+    { key: "google", name: "Google" },
+    { key: "microsoft", name: "Microsoft" },
+    { key: "goldmansachs", name: "Goldman Sachs" },
+    { key: "jpmc", name: "JPMorgan Chase" },
+    { key: "deutschebank", name: "Deutsche Bank" },
+    { key: "bny", name: "BNY Mellon" },
+    { key: "accenture", name: "Accenture" },
+    { key: "pwc", name: "PwC" },
+    { key: "willis", name: "Willis Towers Watson" },
+    { key: "media", name: "Media.net" },
+    { key: "tigeranalytics", name: "Tiger Analytics" },
+    { key: "wellsfargo", name: "Wells Fargo" },
+    { key: "openlm", name: "OpenLM" },
+    { key: "natwest", name: "NatWest Group" },
+    { key: "jktech", name: "JK Tech" },
+    { key: "hcl", name: "HCL Tech" },
+    { key: "tcs", name: "Tata Consultancy Services (TCS)" }
+];
+export default function RecruitersCmp() {
+    return (
+        <section className="RecruitersCmp">
+            <div className="TitleText">Our Recruiters</div>
+            <div className="recGrid">
+                {recruiterData.map((company, index) => (
+                    <Tooltip title={company.name} key={index} placement="top" color="var(--green)">
+                        <Card
+                            hoverable
+                            className="company-card"
+                            cover={
+                                <img
+                                    alt={company.name}
+                                    src={`../../assets/recruiters/${company.key}.webp`}
+                                    className="company-logo"
+                                />
+                            }
+                        />
+                    </Tooltip>
+                ))}
+            </div>
+        </section>
+    );
 }
--- client/src/pages/Gallery/Gallery.jsx
+++ client/src/pages/Gallery/Gallery.jsx
@@ -4,19 +4,19 @@ import GalleryGrid from "../../components/Gallery/GalleryGrid";
 
 const Gallery = () => {
     const imgArr = [
-        {imgClass:"cl1",imgSrc:"public/assets/gallery/gallery-1.webp"},
-        {imgClass:"cl2",imgSrc:"public/assets/gallery/gallery-3.webp"},
-        {imgClass:"cl3",imgSrc:"public/assets/gallery/gallery-13.webp"},
-        {imgClass:"cl4",imgSrc:"public/assets/gallery/gallery-4.webp"},
-        {imgClass:"cl5",imgSrc:"public/assets/gallery/gallery-5.webp"},
-        {imgClass:"cl6",imgSrc:"public/assets/gallery/gallery-6.webp"},
-        {imgClass:"cl7",imgSrc:"public/assets/gallery/gallery-7.webp"},
-        {imgClass:"cl8",imgSrc:"public/assets/gallery/gallery-8.webp"},
-        {imgClass:"cl9",imgSrc:"public/assets/gallery/gallery-9.webp"},
-        {imgClass:"cl10",imgSrc:"public/assets/gallery/gallery-10.webp"},
-        {imgClass:"cl11",imgSrc:"public/assets/gallery/gallery-11.webp"},
-        {imgClass:"cl12",imgSrc:"public/assets/gallery/gallery-12.webp"},
-        {imgClass:"cl13",imgSrc:"public/assets/gallery/gallery-2.webp"}
+        {imgClass:"cl1",imgSrc:"/assets/gallery/gallery-1.webp"},
+        {imgClass:"cl2",imgSrc:"/assets/gallery/gallery-3.webp"},
+        {imgClass:"cl3",imgSrc:"/assets/gallery/gallery-13.webp"},
+        {imgClass:"cl4",imgSrc:"/assets/gallery/gallery-4.webp"},
+        {imgClass:"cl5",imgSrc:"/assets/gallery/gallery-5.webp"},
+        {imgClass:"cl6",imgSrc:"/assets/gallery/gallery-6.webp"},
+        {imgClass:"cl7",imgSrc:"/assets/gallery/gallery-7.webp"},
+        {imgClass:"cl8",imgSrc:"/assets/gallery/gallery-8.webp"},
+        {imgClass:"cl9",imgSrc:"/assets/gallery/gallery-9.webp"},
+        {imgClass:"cl10",imgSrc:"/assets/gallery/gallery-10.webp"},
+        {imgClass:"cl11",imgSrc:"/assets/gallery/gallery-11.webp"},
+        {imgClass:"cl12",imgSrc:"/assets/gallery/gallery-12.webp"},
+        {imgClass:"cl13",imgSrc:"/assets/gallery/gallery-2.webp"}
     ]
     return (
         <>
